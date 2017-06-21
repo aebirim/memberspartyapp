b8f60b0c2aee055fd7bc3e5317ee4c47f4aaeecb
ANGELA EBIRIM <ebirimac@gmail.com>
2017-06-21 02:21:10 +0100

    initial commit

---
+++ lib/generategraph.py
@@ -0,0 +1,63 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.switch_backend("TkAgg")
+
+
+class GenerateGraph:
+
+  def __init__(self, members):
+      self.members = members
+      self.name = []
+      self.party = []
+      self.house = []
+      self.gender = []
+      self.dataMember = [self.name, self.party, self.house, self.gender]
+      self.dataDict = {}
+
+  def test(self):
+      members = self.members['Members']['Member']
+      members_data = members
+      name, gender, party, house = self.buildData(members_data)
+      dataDict = self.buildDict(name, gender, party, house)
+      self.buildGraph(dataDict)
+
+  def buildData(self, data):
+      for member in data:
+          if self.name in self.dataMember:
+             self.name.append(member['DisplayAs'])
+          if self.gender in self.dataMember:
+             self.gender.append(member['Gender'])
+          if self.party in self.dataMember:
+             self.party.append(member['Party']['#text'])
+          if self.house in self.dataMember:
+             self.house.append(member['House'])
+
+      return self.name, self.gender, self.party, self.house
+
+  def buildDict(self, name, gender, party, house):
+      self.dataDict['member'] = name
+      self.dataDict['gender'] = gender
+      self.dataDict['party'] = party
+      self.dataDict['house'] = house
+      #print(self.dataDict)
+      return self.dataDict
+
+
+  def buildGraph(self, dict):
+      politicians = pd.DataFrame(dict)
+      p_house = politicians['house']
+      house = p_house.unique()
+      e = politicians.groupby(['party', 'gender']).size().unstack().plot(kind='barh', stacked=True,
+                                                                         title='Gender count in House of %s' %
+                                                                               house[0], figsize=(19,10))
+      e.set_xlabel("count")
+      e.set_ylabel("party")
+      e.legend(["Female", "Male"], loc=9, ncol=4, fontsize = 'large')
+      figure = e.get_figure()
+      figure.savefig("test.png", dpi=200)
+      #plt.show()
+
+
+
+
---
+++ lib/setup.py
@@ -0,0 +1,44 @@
+from generategraph import GenerateGraph
+import json
+import requests
+import pprint
+from urllib.parse import urljoin
+
+class SetupGraph:
+  """Setup a graph on a page that has the following properties:
+  Attributes:
+      Houses: A string representing the particular house of parliament, Houses=Commons passed as default
+  """
+  headers = {"content-type": "application/json"}
+  baseUrl = "http://data.parliament.uk/membersdataplatform/services/mnis/members/query/"
+  search_constraints = "House=Commons"
+
+  def __init__(self, search_constraints=search_constraints):
+      self.baseUrl = SetupGraph.baseUrl
+      self.search_constraints = search_constraints
+      self.outputs = "Constituencies"
+
+  def _connect(self):
+      url = self._buildUrl()
+      try:
+        r = requests.get(url, headers=SetupGraph.headers)
+      except ValueError as err:
+        print('Get request broken: {}'.format(err))
+        #r.encoding = "utf-8-sig"
+        r.encoding = "utf-8"
+
+      output = r.text
+      return output
+
+  def newGraph(self):
+      output = self._connect()
+      members = json.loads(output[1:])
+      GenerateGraph(members).test()
+
+  def _buildUrl(self):
+      params = self.search_constraints + '/' + self.outputs
+      new_url = urljoin(self.baseUrl, params)
+      return new_url
+
+graph = SetupGraph()
+graph.newGraph()
